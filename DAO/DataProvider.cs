60a99bd52263e5766762f4a8694a2c387f203fcc
Tai Le <letai1854@gmail.com>
2024-05-03 14:41:58 +0700

    no message

--- DAO/DataProvider.cs
+++ DAO/DataProvider.cs
@@ -21,7 +21,8 @@ namespace DAO
         }
         private DataProvider() { }
 
-        private readonly string connectionSTR = @"Data Source = .\SQLEXPRESS;Initial Catalog = QuanLyNhaHang; Integrated Security = True; TrustServerCertificate=True";
+        //private readonly string connectionSTR = @"Data Source = .\SQLEXPRESS;Initial Catalog = QuanLyNhaHang; Integrated Security = True; TrustServerCertificate=True";
+        private readonly string connectionSTR = @"Data Source= .\SQLEXPRESS;Initial Catalog=QuanLyNhaHang;Integrated Security=True;TrustServerCertificate=True";
         
         // Dùng để chạy câu query bình thường hoặc stored procedure
         public DataTable ExecuteQuery(string query, object[] parameter = null)
